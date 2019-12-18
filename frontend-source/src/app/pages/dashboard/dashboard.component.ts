7004fe5b1bd76fd491dc1029f7c3752338ca544b
Julian Schmidt <julian.schmidt@innogy.com>
2019-12-18 16:37:55 +0100

    start display of profits

--- frontend-source/src/app/pages/dashboard/dashboard.component.ts
+++ frontend-source/src/app/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {History, HistorysService} from '../../services/historys.service';
+import {take} from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  history: Array<History> = [];
+  interval;
 
-  ngOnInit() {
+  constructor(private historyService: HistorysService) {
   }
 
+  ngOnInit() {
+    this.historyService.getHistorys().pipe(take(1)).subscribe(histories => {
+      this.history = histories;
+    });
+    const self = this;
+    this.interval = setInterval(() => {
+      self.historyService.getHistorys().pipe(take(1)).subscribe(histories => {
+        self.history = histories;
+      });
+    }, 10000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
 }
+
+
